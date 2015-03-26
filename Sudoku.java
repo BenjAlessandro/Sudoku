dada642634b95f62a767963c79584f6e0a8ab7ef
Benja Cornejo <benjalessandro@outlook.com>
2015-03-26 09:30:08 -0300

    Creación de main

---
+++ Sudoku.java
@@ -0,0 +1,15 @@
+package sudoku;
+
+/*
+ * @author Benja
+ */
+public class Sudoku {
+
+    public static void main(String[] args) {
+        
+        Interfaz i = new Interfaz();
+        i.pack();
+        i.setLocationRelativeTo(null);
+        i.setVisible(true);
+    }
+}
